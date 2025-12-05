f465c3383891141180437911e9af4a7cd39f56c8
korben.dong <ghostyuan@outlook.com>
2025-12-05 18:25:17 +0800

    add dpc3 & ipc1 code

---
+++ run-test.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+li1_prefs=("barca" "bip" "d_jolt" "eip" "fnlmma" "mana" "next_line" "no" "pips" "tap")
+for ((i = 0; i < ${#li1_prefs[@]}; i++)); do
+  pref=${li1_prefs[$i]}
+  ./build_champsim.sh bimodal ${pref} no no no lru 2
+  ./run_2core.sh bimodal-${pref}-no-no-no-lru-2core 1 10 0 ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz
+done
+
+
+
+l1d_prefs=("pangloss" "bingo" "mlop" "team_12" "T_SKID190509.021" "sangam" "ipcp" "berti")
+for ((i = 0; i < ${#l1d_prefs[@]}; i++)); do
+  pref=${l1d_prefs[$i]}
+  ./build_champsim.sh bimodal no ${pref} no no lru 2
+  ./run_2core.sh bimodal-no-${pref}-no-no-lru-2core 1 10 0 ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz
+done
+
+
+
+l2c_prefs=("pangloss" "team_12" "spp.021" "sangam" "ipcp" "berti")
+for ((i = 0; i < ${#l2c_prefs[@]}; i++)); do
+  pref=${l2c_prefs[$i]}
+  ./build_champsim.sh bimodal no no ${pref} no lru 2
+  ./run_2core.sh bimodal-no-no-${pref}-no-lru-2core 1 10 0 ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz
+done
+
+
+
+
+llc_prefs=("sangam" "ipcp" "team_12")
+for ((i = 0; i < ${#llc_prefs[@]}; i++)); do
+  pref=${llc_prefs[$i]}
+  ./build_champsim.sh bimodal no no no ${pref} lru 2
+  ./run_2core.sh bimodal-no-no-no-${pref}-lru-2core 1 10 0 ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz ../traces/spec2k17/602.gcc_s-1850B.champsimtrace.xz
+done
